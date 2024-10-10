12d0eb574efacf0372cd83939f00394dd520466f
Pascal Eberhard <35179162+paeber@users.noreply.github.com>
2024-10-10 15:44:15 +0200

    Add rx/rx structure

--- paekli-rs/src/main.rs
+++ paekli-rs/src/main.rs
@@ -1,13 +1,44 @@
 use clap::Parser;
+use clap::Subcommand;
+
+
+
+
+#[derive(Subcommand)]
+enum Command {
+    /// Send a paekli to a friend
+    Send,
+    /// Receive a paekli from a friend
+    Receive,
+}
 
 /// send and receive joy with ✨ paekli-cli ✨
 #[derive(Parser)]
 #[clap(version)]
-struct Cli;
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+
+const SEND_MESSAGE: &str = "\
+Thank you for trusting Paekli LLC!
+We will deliver your paekli in mint condition.
+* throws your paekli directly in the trash *";
+
+const RECEIVE_MESSAGE: &str = "\
+There aren't any paekli for you at the moment.
+* tries to hide paekli in the trash can *";
 
 fn main() {
-    let _args = Cli::parse();
-    println!("Paekli LLC is currentli closed 😢");
+    let args = Cli::parse();
+
+    match args.command {
+        Command::Send => println!("{SEND_MESSAGE}"),
+        Command::Receive => println!("{RECEIVE_MESSAGE}"),
+    } 
+
+
 }
 
 #[test]
